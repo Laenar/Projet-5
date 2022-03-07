9cea97e98e7b66d4ab017f5f62ec4f3fb7327988
Laenar <florian.meu@gmail.com>
2022-03-07 09:39:34 +0100

    Validation script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,9 @@
-let workSection = document.getElementById('items');
-let textFormat = [];
+let workSection = document.getElementById('items'); // Varriable de travaile principale
 
 get_Products ();
 
-function get_Products () {
+function get_Products () //Recupere l’emseble des produits
+{
   fetch("http://localhost:3000/api/products")
   .then(function(res) {
     if (res.ok) {
@@ -12,25 +12,19 @@ function get_Products () {
   })
   .then(function(value) {
     read_Product(value);
-    console.log(value);
   })
   .catch(function(err) {
     console.log(err);
   });
 }
 
-function read_Product (product) {
+function read_Product (product) // Ajoute les produits 1 à 1 sur la page (Utilise la var workSection)
+{
   product.forEach(element => {
-    print_Product (element);
-  });
-}
-
-function print_Product (product) {
-  let codeItem = ' <article> <img src="'+product['imageUrl']+'" alt="'+product['altTxt']+'"> <h3 class="productName">'+product['name']+'</h3> <p class="productDescription">'+product['description']+'</p></article>';
-  
-  textFormat.push(codeItem);
-  let newElem = document.createElement("a");
-  newElem.setAttribute("href","product.html?id="+product['_id']);
+    let codeItem = ' <article> <img src="'+element['imageUrl']+'" alt="'+element['altTxt']+'"> <h3 class="productName">'+element['name']+'</h3> <p class="productDescription">'+element['description']+'</p></article>';
+    let newElem = document.createElement("a");
+    newElem.setAttribute("href","product.html?id="+element['_id']);
     newElem.innerHTML = codeItem;
     workSection.appendChild(newElem);
+  });
 }
